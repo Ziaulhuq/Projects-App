70273c30e6e14d991272c3dc0d304ccc6a80cc49
ziaulhuq <ziaulhuqbe@gmail.com>
2024-01-29 13:07:06 +0800

    updating Task input

--- src/Components/Tasks/InputTasks.jsx
+++ src/Components/Tasks/InputTasks.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputTasks = () => {
+  const [inputTask, setInputTask] = useState("");
+
+  function handleInputTask(e) {
+    setInputTask(e.target.value);
+  }
+
+  function handleAddTask() {
+    setInputTask("");
+  }
+
   return (
-    <div className="flex align-baseline">
-      <input type="text" className="w-[20rem] p-3 mb-4 h-9 bg-red-100" />
-      <button className="mx-2 -mt-3 text-slate-800 font-medium">
+    <div className="flex gap-2">
+      <input
+        type="text"
+        className="w-[20rem] p-3 mb-4 h-9 bg-red-100 rounded-md"
+        value={inputTask}
+        onChange={handleInputTask}
+      />
+      <button
+        className="mx-2 rounded-md bg-slate-600 text-slate-100 font-medium px-2 py-1 h-9"
+        onClick={handleAddTask}
+      >
         Add Tasks
       </button>
     </div>
